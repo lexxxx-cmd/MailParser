f2d1cbf8176229582d2ece6e93c0e16567f4e2df
leon <13628189941lyl@gmail.com>
2025-07-08 16:31:08 +0800

    feat 添加相机预览|单次采集|保存

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <MVGigE.h>
+#include <MVImageC.h>
+#include <MVCamProptySheet.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +19,34 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    // 显示函数
+    int showStreamOnLabel(MV_IMAGE_INFO* pInfo);
+
+private slots:
+    void on_ButtonShot_clicked();
+
+    void on_ButtonOpen_clicked();
+
+
+
+
+
+    void on_ButtonStop_clicked();
+
+
+    void on_Property_clicked();
+
+    void on_ButtonSave_clicked();
 
 private:
     Ui::MainWindow *ui;
+    // 相机初始化 构造
+    void initialCamera();
+    void drawImage();
+    HANDLE m_hCam;
+    HANDLE m_hImg;
+    HANDLE m_hPropDlg;
+
+    bool m_bRun;
 };
 #endif // MAINWINDOW_H
